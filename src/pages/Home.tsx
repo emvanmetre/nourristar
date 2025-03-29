779ae0964a695cf7ffe9a61c548f0743d8811f09
Elizabeth <elizabethmvm@gmail.com>
2025-03-29 16:00:58 -0400

    add material ui and test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}))
 
 const Home = () => {
   document.body.classList.add('bg-light')
-  return <div style={{ width: '50%', paddingLeft: '25%' }}></div>
+  return (
+    <Grid container spacing={2}>
+      <Grid size={8}>
+        <Item>size=8</Item>
+      </Grid>
+      <Grid size={4}>
+        <Item>size=4</Item>
+      </Grid>
+      <Grid size={4}>
+        <Item>size=4</Item>
+      </Grid>
+      <Grid size={8}>
+        <Item>size=8</Item>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Home
