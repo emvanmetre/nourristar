69578d8ddae91fb55799e08e1e24f676ac499cc9
Elizabeth <elizabethmvm@gmail.com>
2025-03-29 16:02:53 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 // import logo from './logo.svg';
 import './App.css'
+import './style.css'
+import React, { useLayoutEffect } from 'react'
 import { Home } from './pages'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+
+const ScrollToTop = () => {
+  const location = useLocation()
+
+  useLayoutEffect(() => {
+    // Scroll to the top of the page when the route changes
+    window.scrollTo({ top: 0, left: 0 })
+  }, [location.pathname])
+
+  return null
+}
 
 function App() {
-  return <Home></Home>
+  return (
+    <Router>
+      {/* <Navbar /> */}
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Home />} />
+        <Route path="/create" element={<Home />} />
+        <Route path="/component/:id" element={<Home />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
