1ca92027fdfa40a99b47c8e8594a0f0fc37e42cb
Emily Chen <qeq3cf@virginia.edu>
2025-03-29 21:06:07 -0400

    more testing db features

--- test.js
+++ test.js
@@ -1,20 +1,48 @@
 import { connect, disconnect } from 'mongoose'
-import { User } from './server/schemas.js'
+import { User, Recipe } from './server/schemas.js'
 
 // Connect to MongoDB
-connect('mongodb://localhost:27017/nourristar', {
-}).then(async () => {
-  console.log("Connected to MongoDB")
+connect('mongodb://localhost:27017/nourristar')
+  .then(async () => {
+    console.log("Connected to MongoDB")
+    
+    // get list of all users (testing persistence)
+    try {
+      const allUsers = await User.find({})
+      console.log("List of all users:", allUsers)
+    } catch (error) {
+      console.error("Error fetching users:", error)
+    }
 
-  // Create a new user
-  const newUser = new User({ username: "testuser", password: "testpassword" })
+    // create a new user
+    ////////////!!! just change the username so it doesnt add duplicates
+    const newUser = new User({ username: "testuser1", password: "testpassword" })
+    let savedUser;
 
-  try {
-    const savedUser = await newUser.save()
-    console.log("User saved successfully! :", savedUser)
-  } catch (error) {
-    console.error("Error saving user:", error)
-  }
+    try {
+      savedUser = await newUser.save()
+      console.log("User saved successfully!", savedUser)
+    } catch (error) {
+      console.error("Error saving user:", error)
+      disconnect()
+      return
+    }
 
-  disconnect()
-}).catch(err => console.error("MongoDB connection error:", err))
+    // new recipe with prev userid
+    const newRecipe = new Recipe({
+      userId: savedUser._id,
+      text: "banana bread",
+      tags: ["dessert", "vegetarian"],
+      picture: "https://example.com/test-recipe.jpg"
+    })
+
+    try {
+      const savedRecipe = await newRecipe.save()
+      console.log("Recipe saved successfully!", savedRecipe)
+    } catch (error) {
+      console.error("Error saving recipe:", error)
+    }
+
+    disconnect()
+  })
+  .catch(err => console.error("MongoDB connection error:", err))
